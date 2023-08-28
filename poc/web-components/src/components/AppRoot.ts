b0a43cb37fd8d5ba0403f6005720e99c47396a64
Gyumin Choi <choegyumin@gmail.com>
2023-08-29 00:53:43 +0900

    Write sample (Batch update)

--- poc/web-components/src/components/AppRoot.ts
+++ poc/web-components/src/components/AppRoot.ts
@@ -16,6 +16,10 @@ export default class AppRoot extends WebComponent {
     return this.fragment.querySelector<HTMLElement>(':host [part="add"]');
   }
 
+  get batchAddNode() {
+    return this.fragment.querySelector<HTMLElement>(':host [part="batchAdd"]');
+  }
+
   #onHelloClick = () => {
     alert('Hi');
   };
@@ -24,14 +28,25 @@ export default class AppRoot extends WebComponent {
     this.state.list = [...this.state.list, this.state.list.length];
   };
 
+  #onBatchAddClick = () => {
+    for (let i = 0; i < 1000; i += 1) {
+      this.state.list = [0];
+      for (let j = 1; j < 10; j += 1) {
+        this.state.list = [...this.state.list, this.state.list.length];
+      }
+    }
+  };
+
   addEventListeners() {
     this.helloNode?.addEventListener('click', this.#onHelloClick);
     this.addNode?.addEventListener('click', this.#onAddClick);
+    this.batchAddNode?.addEventListener('click', this.#onBatchAddClick);
   }
 
   removeEventListeners() {
     this.helloNode?.removeEventListener('click', this.#onHelloClick);
     this.addNode?.removeEventListener('click', this.#onAddClick);
+    this.batchAddNode?.removeEventListener('click', this.#onBatchAddClick);
   }
 
   willUpdate() {
@@ -56,6 +71,7 @@ export default class AppRoot extends WebComponent {
         <hr>
         <h2>Properties</h2>
         <my-button part="add">+</my-button>
+        <my-button part="batchAdd">+++</my-button>
         <my-list list='${JSON.stringify(this.state.list)}'></my-list>
       </div>
     `;
